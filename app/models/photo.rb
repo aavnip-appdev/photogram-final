3f2b700382a88e8c8abccee1083d4dda99da0f41
aavnip <aavnip@chicagobooth.edu>
2022-03-13 19:39:42 +0000

    Added all associations for photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -12,4 +12,18 @@
 #  owner_id       :integer
 #
 class Photo < ApplicationRecord
+  # from ideas first draft - additional validations
+  validates(:owner_id, { :presence => true })
+  validates(:image, { :presence => true })
+
+  # from ideas first draft - direct associations
+  belongs_to(:owner, { :class_name => "User", :counter_cache => :own_photos_count })
+  has_many(:likes, { :dependent => :destroy })
+  has_many(:comments, { :dependent => :destroy })
+
+  # from ideas first draft - indirect associations
+  has_many(:fans, { :through => :likes, :source => :user })
+  has_many(:followers, { :through => :owner, :source => :following })
+  has_many(:fan_followers, { :through => :fans, :source => :following })
+
 end
