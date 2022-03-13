d3ae15ebbade11fac09b297360f84184032d3258
aavnip <aavnip@chicagobooth.edu>
2022-03-13 19:43:00 +0000

    Added all associations for likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -9,4 +9,13 @@
 #  photo_id   :integer
 #
 class Like < ApplicationRecord
+  # from ideas first draft - additional validations
+  validates(:photo_id, { :presence => true })
+  validates(:photo_id, { :uniqueness => { :scope => ["fan_id"], :message => "already liked" } })
+  validates(:fan_id, { :presence => true })
+
+  # from ideas first draft - direct associations
+  belongs_to(:user, { :foreign_key => "fan_id", :counter_cache => true })
+  belongs_to(:photo, { :counter_cache => true })
+
 end
