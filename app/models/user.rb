773a9ba5a23391b35c0de7b5c931e846d1327eb3
aavnip <aavnip@chicagobooth.edu>
2022-03-13 19:36:00 +0000

    Added all associations for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,26 @@
 #  updated_at                     :datetime         not null
 #
 class User < ApplicationRecord
+  # from draft account generator
   validates :email, :uniqueness => { :case_sensitive => false }
   validates :email, :presence => true
   has_secure_password
+
+  # from ideas first draft - additional username validations
+  validates(:username, { :presence => true })
+  validates(:username, { :uniqueness => true })
+
+  # from ideas first draft - direct associations
+  has_many(:likes, { :foreign_key => "fan_id", :dependent => :destroy })
+  has_many(:comments, { :foreign_key => "commenter_id", :dependent => :destroy })
+  has_many(:sent_follow_requests, { :class_name => "FollowRequest", :foreign_key => "sender_id", :dependent => :destroy })
+  has_many(:received_follow_requests, { :class_name => "FollowRequest", :foreign_key => "recipient_id", :dependent => :destroy })
+  has_many(:own_photos, { :class_name => "Photo", :foreign_key => "owner_id", :dependent => :destroy })
+
+  # from ideas first draft - indirect associations
+  has_many(:following, { :through => :sent_follow_requests, :source => :recipient })
+  has_many(:followers, { :through => :received_follow_requests, :source => :sender })
+  has_many(:liked_photos, { :through => :likes, :source => :photo })
+  has_many(:feed, { :through => :following, :source => :own_photos })
+  has_many(:activity, { :through => :following, :source => :liked_photos })
 end
