c5f76e445ffc84852060a1f1bfde091d09aeacf3
aavnip <aavnip@chicagobooth.edu>
2022-03-13 19:53:23 +0000

    Readded associations for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,4 +10,12 @@
 #  photo_id   :integer
 #
 class Comment < ApplicationRecord
+  # from ideas first draft - additional validations
+  validates(:photo_id, { :presence => true })
+  validates(:body, { :presence => true })
+  validates(:author_id, { :presence => true })
+
+  # from ideas first draft - direct associations
+  belongs_to(:commenter, { :class_name => "User", :foreign_key => "author_id", :counter_cache => true })
+  belongs_to(:photo, { :counter_cache => true })
 end
